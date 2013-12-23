b688911c59aa926c1675cf71586eadafe4febc78
Kevin Ushey <kevinushey@gmail.com>
2013-12-22 21:26:07 -0800

    fixed sugar table

--- inst/include/Rcpp/sugar/functions/table.h
+++ inst/include/Rcpp/sugar/functions/table.h
@@ -2,25 +2,27 @@
 //
 // table.h: Rcpp R/C++ interface class library -- table match
 //
-// Copyright (C) 2012   Dirk Eddelbuettel and Romain Francois
+// Copyright (C) 2012 - 2013   Dirk Eddelbuettel, Romain Francois, and Kevin Ushey
 //
-// This file is part of Rcpp98.
+// This file is part of Rcpp.
 //
-// Rcpp98 is free software: you can redistribute it and/or modify it
+// Rcpp is free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
 //
-// Rcpp98 is distributed in the hope that it will be useful, but
+// Rcpp is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Rcpp98.  If not, see <http://www.gnu.org/licenses/>.
+// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef Rcpp__sugar__table_h
 #define Rcpp__sugar__table_h
+
+#include <Rcpp/sugar/tools/mapcompare.h>
           
 namespace Rcpp{
 namespace sugar{
@@ -28,7 +30,7 @@ namespace sugar{
 template <typename HASH, typename STORAGE>
 class CountInserter {
 public:
-    CountInserter( HASH& hash_ ) : hash(hash_), index(0) {}
+    CountInserter( HASH& hash_ ) : hash(hash_) {}
     
     inline void operator()( STORAGE value ){
         hash[value]++ ;
@@ -36,7 +38,6 @@ public:
     
 private:
     HASH& hash ;
-    int index;
 } ; 
 
 template <typename HASH, int RTYPE>
@@ -61,33 +62,24 @@ class Table {
 public:
     typedef typename Rcpp::traits::storage_type<RTYPE>::type STORAGE ;
     
-    Table( const TABLE_T& table ): hash(), map() {
-        // populate the initial hash
+    Table( const TABLE_T& table ): hash() {
         std::for_each( table.begin(), table.end(), Inserter(hash) ) ;
-        
-        // populate the map, sorted by keys
-        map.insert( hash.begin(), hash.end() ) ;
     }
     
     inline operator IntegerVector() const { 
-        // fill the result
-        int n = map.size() ;
+        int n = hash.size() ;
         IntegerVector result = no_init(n) ;
         CharacterVector names = no_init(n) ;
-        std::for_each( map.begin(), map.end(), Grabber<SORTED_MAP,RTYPE>(result, names) ) ;
+        std::for_each( hash.begin(), hash.end(), Grabber<HASH, RTYPE>(result, names) ) ;
         result.names() = names ;
         return result ;
     }
     
 private:
-    typedef RCPP_UNORDERED_MAP<STORAGE, int> HASH ;
+    typedef std::map<STORAGE, int, MapCompare<STORAGE> >HASH ;
     typedef CountInserter<HASH,STORAGE> Inserter ;
-    
-    typedef std::map<STORAGE, int, typename Rcpp::traits::comparator_type<RTYPE>::type > SORTED_MAP ;
-    
     HASH hash ;
-    SORTED_MAP map ;
-}; 
+};
     
 } // sugar
 
