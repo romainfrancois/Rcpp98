f30f3c988bb8e4ba757ad50d7b2c1378cbfc3699
Kevin Ushey <kevinushey@gmail.com>
2013-12-26 14:30:06 -0800

    Rcpp -> Rcpp98

--- inst/include/Rcpp/sugar/functions/table.h
+++ inst/include/Rcpp/sugar/functions/table.h
@@ -1,23 +1,23 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 8 -*-
 //
-// table.h: Rcpp R/C++ interface class library -- table match
+// table.h: Rcpp98 R/C++ interface class library -- table match
 //
 // Copyright (C) 2012 - 2013   Dirk Eddelbuettel, Romain Francois, and Kevin Ushey
 //
-// This file is part of Rcpp.
+// This file is part of Rcpp98.
 //
-// Rcpp is free software: you can redistribute it and/or modify it
+// Rcpp98 is free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
 //
-// Rcpp is distributed in the hope that it will be useful, but
+// Rcpp98 is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
+// along with Rcpp98.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef Rcpp__sugar__table_h
 #define Rcpp__sugar__table_h
