ce3917e56753b5d163e422f46e746d67b1fec1eb
Romain François <romain@r-enthusiasts.com>
2014-01-22 00:08:53 +0100

    fix the String = NA. bug

--- inst/include/Rcpp/Na_Proxy.h
+++ inst/include/Rcpp/Na_Proxy.h
@@ -1,20 +1,3 @@
-// Copyright (C) 2013 Romain Francois
-//
-// This file is part of Rcpp98.
-//
-// Rcpp98 is free software: you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-//
-// Rcpp98 is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Rcpp98.  If not, see <http://www.gnu.org/licenses/>.
-
 #ifndef Rcpp_Na_Proxy_h
 #define Rcpp_Na_Proxy_h
 
@@ -57,6 +40,15 @@ namespace Rcpp{
             _["double"] = NA == NA_REAL, 
             _["string"] = NA == NA_STRING 
             ) ;
+    }
+    
+    inline String::String( Na_Proxy ) : String(NA_STRING){}
+    
+    inline String& String::operator=( Na_Proxy ) {
+      data = NA_STRING ;
+      buffer_ready = false ;
+      valid = true ;
+      return *this ;  
     }
 }
 #endif
